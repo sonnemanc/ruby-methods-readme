202f1096362564ba353ae2ebe0afe276b524b19b
Corban <sonnemanc@gmail.com>
2021-07-11 15:07:43 -0600

    Done with Assignment

--- lib/example.rb
+++ lib/example.rb
@@ -1,4 +1,13 @@
 # Use this space to code along with the readme
+def greeting
+    puts "Hello World"
+  end
+  
+  greeting
+  greeting
+  greeting
+  greeting
+  greeting 
 
 phrase = "Hello World!"
 puts phrase
